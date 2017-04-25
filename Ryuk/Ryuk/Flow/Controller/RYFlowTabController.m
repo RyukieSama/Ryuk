3b7abc2c81f9b98afe89f7dc147c2e8fd01d32ac
Ryukie <ryukiewang@qq.com>
2017-04-25 17:54:28 +0800

    no message

--- Ryuk/Ryuk/Flow/Controller/RYFlowTabController.m
+++ Ryuk/Ryuk/Flow/Controller/RYFlowTabController.m
@@ -12,9 +12,14 @@
 
 #define API_MY_TIMELINE @"https://api.weibo.com/2/statuses/home_timeline.json"
 
-@interface RYFlowTabController ()
+@interface RYFlowTabController ()<UITableViewDelegate,UITableViewDataSource>
 
-
+@property (nonatomic, assign) long long since_id;
+@property (nonatomic, assign) long long max_id;
+@property (nonatomic, assign) NSInteger page;
+@property (nonatomic, assign) NSInteger feature;
+@property (nonatomic, strong) NSMutableArray <RYStatuse *>*statuses;
+@property (nonatomic, strong) UITableView *tvFlow;
 
 @end
 
@@ -25,6 +30,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self setupUI];
+    [self setDefault];
 }
 
 - (void)dealloc {
@@ -36,6 +42,12 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - data
+- (void)setDefault {
+    self.page = 1;
+    self.feature = 0;
+}
+
 #pragma mark - UI
 - (void)setupUI {
     self.title = @"微博";
@@ -43,29 +55,43 @@
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    //    access_token	true	string	采用OAuth授权方式为必填参数，OAuth授权后获得。
-    //    since_id	false	int64	若指定此参数，则返回ID比since_id大的微博（即比since_id时间晚的微博），默认为0。
-    //    max_id	false	int64	若指定此参数，则返回ID小于或等于max_id的微博，默认为0。
-    //    count	false	int	单页返回的记录条数，最大不超过100，默认为20。
-    //    page	false	int	返回结果的页码，默认为1。
-    //    base_app	false	int	是否只获取当前应用的数据。0为否（所有数据），1为是（仅当前应用），默认为0。
-    //    feature	false	int	过滤类型ID，0：全部、1：原创、2：图片、3：视频、4：音乐，默认为0。
-    //    trim_user	false	int	返回值中user字段开关，0：返回完整user字段、1：user字段仅返回user_id，默认为0。
+    __weak typeof(self) weakSelf = self;
     [RYNetworkManager ry_getWithUrl:API_MY_TIMELINE
                   requestDictionary:@{
-                                      @"since_id" : @0,
-                                      @"max_id" : @0,
-                                      @"count" : @20,
-                                      @"page" : @1,
-                                      @"base_app" : @0,
-                                      @"feature" : @0,
-                                      @"trim_user" : @0
+                                      @"since_id" : @(self.since_id),//若指定此参数，则返回ID比since_id大的微博（即比since_id时间晚的微博），默认为0。
+                                      @"max_id" : @(self.max_id),//若指定此参数，则返回ID小于或等于max_id的微博，默认为0。
+                                      @"count" : @20,//单页返回的记录条数，最大不超过100，默认为20。
+                                      @"page" : @(self.page),//返回结果的页码，默认为1。
+                                      @"base_app" : @0,//是否只获取当前应用的数据。0为否（所有数据），1为是（仅当前应用），默认为0。
+                                      @"feature" : @(self.feature),//过滤类型ID，0：全部、1：原创、2：图片、3：视频、4：音乐，默认为0。
+                                      @"trim_user" : @0 //返回值中user字段开关，0：返回完整user字段、1：user字段仅返回user_id，默认为0。
                                       }
                       responseModel:[RYFlowPageModel class]
                            useCache:NO
                   completionHandler:^(id data) {
-                      NSLog(@"kkk===%@",data);
+                      //TODO: 错误处理
+                      weakSelf.since_id = ((RYFlowPageModel *)data).since_id;
+                      weakSelf.max_id = ((RYFlowPageModel *)data).max_id;
+                      weakSelf.statuses = ((RYFlowPageModel *)data).statuses.mutableCopy;
                   }];
+}
+
+#pragma mark - tableVIew
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+
+
+#pragma mark - lazy
+- (UITableView *)tvFlow {
+    if (!_tvFlow) {
+        _tvFlow = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+        _tvFlow.delegate = self;
+        _tvFlow.dataSource = self;
+        _tvFlow.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    }
+    return _tvFlow;
 }
 
 @end
