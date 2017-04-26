3e12749f715c617a9654e9462990135d5011e751
Ryukie <ryukiewang@qq.com>
2017-04-26 16:38:58 +0800

    初步导入图片浏览控件

---
+++ Ryuk/Ryuk/Components/RYPhotosBrowser/RYImageBrowserScrollView.h
@@ -0,0 +1,16 @@
+//
+//  RYImageBrowserScrollView.h
+//  RYImageBrowser
+//
+//  Created by RongqingWang on 16/11/4.
+//  Copyright © 2016年 RyukieSama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RYImageBrowserScrollView : UIScrollView
+
+@property (nonatomic, strong) UIImage *image;
+@property (nonatomic, strong) UIImageView *imageView;
+
+@end
