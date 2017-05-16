dff5d1f2bf231ebbb3e152af5320e4b7a21cec63
Ryukie <ryukiewang@qq.com>
2017-05-16 11:43:03 +0800

    点击获取到unit

--- Ryuk/Ryuk/Components/RYText/RYLabel/RYLabel.m
+++ Ryuk/Ryuk/Components/RYText/RYLabel/RYLabel.m
@@ -10,7 +10,7 @@
 #import "YYText.h"
 #import "RYTextUnit.h"
 
-@interface RYLabel ()
+@interface RYLabel ()<UIGestureRecognizerDelegate>
 
 @property (nonatomic, strong) NSMutableArray <RYTextUnit *>*arrLinks;
 @property (nonatomic, strong) NSMutableArray <RYTextUnit *>*arrImages;
@@ -23,6 +23,7 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     self.userInteractionEnabled = YES;
+    [self addGestureRecognizers];
     return self;
 }
 
@@ -129,6 +130,90 @@
         }
     }
     return _arrAts;
+}
+
+
+// 将点击的位置转换成字符串的偏移量，如果没有找到，则返回-1
+- (CFIndex)touchContentOffsetInViewAtPoint:(CGPoint)point {
+//    CTFrameRef textFrame = data.ctFrame;
+    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)self.attributedText);
+    
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathAddRect(path, NULL, CGRectMake(0, 0, self.frame.size.width, self.frame.size.height));
+    
+    CTFrameRef frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, 0), path, NULL);
+    CFRelease(path);
+    
+//    CTFrameRef textFrame;
+    CFArrayRef lines = CTFrameGetLines(frame);
+    if (!lines) {
+        return -1;
+    }
+    CFIndex count = CFArrayGetCount(lines);
+    
+    // 获得每一行的origin坐标
+    CGPoint origins[count];
+    CTFrameGetLineOrigins(frame, CFRangeMake(0,0), origins);
+    
+    // 翻转坐标系
+    CGAffineTransform transform =  CGAffineTransformMakeTranslation(0, self.bounds.size.height);
+    transform = CGAffineTransformScale(transform, 1.f, -1.f);
+    
+    CFIndex idx = -1;
+    for (int i = 0; i < count; i++) {
+        CGPoint linePoint = origins[i];
+        CTLineRef line = CFArrayGetValueAtIndex(lines, i);
+        // 获得每一行的CGRect信息
+        CGRect flippedRect = [self getLineBounds:line point:linePoint];
+        CGRect rect = CGRectApplyAffineTransform(flippedRect, transform);
+        
+        if (CGRectContainsPoint(rect, point)) {
+            // 将点击的坐标转换成相对于当前行的坐标
+            CGPoint relativePoint = CGPointMake(point.x-CGRectGetMinX(rect),
+                                                point.y-CGRectGetMinY(rect));
+            // 获得当前点击坐标对应的字符串偏移
+            idx = CTLineGetStringIndexForPosition(line, relativePoint);
+        }
+    }
+    
+    NSLog(@"````````````%ld",idx);
+    return idx;
+}
+
+- (CGRect)getLineBounds:(CTLineRef)line point:(CGPoint)point {
+    CGFloat ascent = 0.0f;
+    CGFloat descent = 0.0f;
+    CGFloat leading = 0.0f;
+    CGFloat width = (CGFloat)CTLineGetTypographicBounds(line, &ascent, &descent, &leading);
+    CGFloat height = ascent + descent;
+    return CGRectMake(point.x, point.y - descent, width, height);
+}
+
+- (void)touchEventAtIndex:(CFIndex)index {
+    //找出在这个额位置的Unit
+    for (RYTextUnit *unit in self.arrAts) {
+        if ((index >= unit.range.location) && (index <= unit.range.location + unit.range.length)) {
+            NSLog(@"%@",unit.content);
+        }
+    }
+}
+
+#pragma mark - Gesture recognizers
+- (void)addGestureRecognizers {
+    UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapping:)];
+    [singleTap setDelegate:self];
+    [self addGestureRecognizer:singleTap];
+}
+
+#pragma mark - Gesture recognizer delegate
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch {
+    return YES;
+}
+
+#pragma mark - Handle tappings
+- (void)handleTapping:(UITapGestureRecognizer *)recognizer {
+    CGPoint point = [recognizer locationInView:self];
+    [self touchEventAtIndex:[self touchContentOffsetInViewAtPoint:point]];
 }
 
 @end
